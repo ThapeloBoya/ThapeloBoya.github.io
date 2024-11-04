f7d327f4349ff4f29915bdc16c064dc042caae1f
ThapeloB <88501835+ThapeloBoya@users.noreply.github.com>
2024-11-04 09:16:49 +0200

    Delete TravelExperience directory

--- TravelExperience/src/components/Nav.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Navbar(){
-    return(
-        <div className="nav-container">
-        <h1 className="logo">ED<span>VEL</span></h1>
-        </div>
-    )
-}
